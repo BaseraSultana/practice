ee0b8393d4351765bf6a7db71f12aac7db9ba284
Basera Sultana <baserasultana50@gmail.com>
2026-08-13 04:14:40 +0530

    problems

---
+++ practice1.py
@@ -0,0 +1,46 @@
+# to find the largest number without max() function.
+# numbers = [12, 45, 7, 89, 23, 56]
+# largest = numbers[0]
+# for i in numbers:
+#     if i > largest:
+#         largest = i
+
+# print(largest)
+
+# TO FIND THE NUMBER OF EVEN NUMBERS IN THE LIST
+# numbers = [2, 7, 10, 13, 18, 21, 24, 31]
+# even = []
+# for i in numbers:
+#     if i % 2 == 0:
+#         even.append(i)
+
+# print(len(even))
+
+#  REVERSE STRING
+# poo = "python"
+# qoo = ""
+# i = len(poo) - 1
+#     while i >= 0:
+#         qoo += poo[i]
+#         i -= 1
+
+# print(qoo)
+# poo = "python"
+# qoo = ""
+# i = 0
+# while i <= len(poo)-1:
+#     qoo += poo[i]
+#     i += 1
+
+# print(qoo)
+# COUNT THE VOWELS IN A STRING
+def count_vowels(text):
+    even = []
+    for num in text:
+        if num in "aeiou":
+            even.append(num)
+
+    print(len(even))
+
+
+count_vowels("cybersecurity")
